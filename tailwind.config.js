2b171e3600c4a35d7c72c9628ce20c14416a8578
axelbol <axel_lp_alvarado@hotmail.com>
2022-05-22 21:45:41 -0400

    Design idea cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,10 @@ module.exports = {
             maxWidth: {
                 custom: '62.5rem',
             },
+            boxShadow: {
+                card: '4px 4px 15px 0 rgba(36, 37, 38, 0.08)',
+                dialog: '3px 4px 15px 0 rgba(36, 37, 38, 0.22)'
+            },
             spacing: {
                 70: '17.5rem',
                 175: '43.75rem'
@@ -34,8 +38,14 @@ module.exports = {
             fontFamily: {
                 sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
             },
+            fontSize: {
+                xxs: ['0.625rem', {lineHeight: '1rem'}],
+            },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+    ],
 };
