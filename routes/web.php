ed6e793f6c7f0ff4ec4d3b951c271b39a5be6999
axelbol <axel_lp_alvarado@hotmail.com>
2022-05-26 11:41:41 -0400

    Design single idea page

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('/idea', 'show');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
